144079cb5cb98c5e91bfd987e624f679e7c1845e
osrodgon <osrodgon@gmail.com>
2025-07-11 15:55:57 +0200

    [Chore]: Migrated code from spanish to english

---
+++ library_server/library/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Book
+from .forms import BookForm
+
+
+def book_list(request):
+    books = Book.objects.all
+    return render(request, 'book/book_list.html', {'books': books})
+
+
+def book_detail(request, book_id):
+    book = get_object_or_404(Book, pk=book_id)
+    return render(request, 'book/book_detail.html', {'book': book})
+
+
+def create_book(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm()
+    return render(request, 'book/create_book.html', {'form': form})
+
+
+def edit_book(request, book_id):
+    book = get_object_or_404(Book, pk=book_id)
+    if request.method == 'POST':
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect('book_detail', book_id=book.id)
+    else:
+        form = BookForm(instance=book)
+    return render(request, 'book/edit_book.html', {'form': form, 'book': book})
+
+
+def delete_book(request, book_id):
+    book = get_object_or_404(Book, pk=book_id)
+    if request.method == 'POST':
+        book.delete()
+        return redirect('book_list')
+    return render(request, 'book/delete_book.html', {'book': book})
+
+
+from django.shortcuts import render
+
+# Create your views here.
